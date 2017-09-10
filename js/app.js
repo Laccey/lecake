5679003d03949e31631ba4d0aea6d0d834998ff9
liang xu <1040664047@qq.com>
2017-09-10 18:45:22 +0800

    修改首页

--- js/app.js
+++ js/app.js
@@ -17,9 +17,9 @@
 // $(document).ready(function () {
 //     setTimeout(scrollTo,0,0,0);
 // });
-window.onload = function () {
-    setTimeout(scrollTo,0,0,0);
-};
+// window.onload = function () {
+//     setTimeout(scrollTo,0,0,0);
+// };
 angular.module('Module',['ng','ngRoute','ngAnimate','ngCookies','infinite-scroll']).config(['$routeProvider',function ($routeProvider) {
     $routeProvider
     //     .when('/start',{
@@ -100,6 +100,112 @@ angular.module('Module',['ng','ngRoute','ngAnimate','ngCookies','infinite-scroll
         centeredSlides: true,
         autoplay: 2500,
         autoplayDisableOnInteraction: false
+    });
+    // $scope.scrollerConfig = {
+    //     useCSSTransition: true,
+    //     gpuOptimization: true,
+    //     pullToRefresh :true,
+    //     pullToRefreshConfig:{
+    //         labelPull:"下拉刷新",
+    //         labelClick:"点击刷新",
+    //         labelRelease:"释放刷新",
+    //         labelUpdate:"刷新中..."
+    //     },
+    //     onPullToRefresh : function(e){
+    //         setTimeout(function () {
+    //             e();
+    //         }, 1500);
+    //     },
+    //     pullToLoadMore:true,
+    //     pullToLoadMoreConfig:{
+    //         labelPull:"上拉加载",
+    //         labelClick:"点击加载",
+    //         labelRelease:"释放加载",
+    //         labelUpdate:"加载中..."
+    //     },
+    //     onPullToLoadMore:function(e){
+    //         setTimeout(function () {
+    //             e();
+    //         }, 1500);
+    //     }
+    // };
+    //
+    // window.scroller = new Scroller('#wrapper', scrollerConfig);
+    $scope.loadFlag = true;
+    $scope.oi = 0;
+    $scope.mySwiper = new Swiper('.outer',{
+        direction: 'vertical',
+        scrollbar: '.swiper-scrollbar',
+        slidesPerView: 'auto',
+        mousewheelControl: true,
+        freeMode: true,
+        onTouchMove: function(swiper){		//手动滑动中触发
+            var _viewHeight = document.getElementsByClassName('outer-wrapper')[0].offsetHeight;
+            var _contentHeight = document.getElementsByClassName('inner-slide')[0].offsetHeight;
+
+
+            if(mySwiper.translate < 50 && mySwiper.translate > 0) {
+                $(".init-loading").html('下拉刷新').show();
+            }else if(mySwiper.translate > 50 ){
+                $(".init-loading").html('松开立即刷新').show();
+            }
+        },
+        onTouchEnd: function(swiper) {
+            var _viewHeight = document.getElementsByClassName('outer-wrapper')[0].offsetHeight;
+            var _contentHeight = document.getElementsByClassName('inner-slide')[0].offsetHeight;
+            // 上拉加载
+            if(mySwiper.translate <= _viewHeight - _contentHeight - 50 && mySwiper.translate < 0) {
+                // console.log("已经到达底部！");
+
+                if(loadFlag){
+                    $(".loadtip").html('正在加载...');
+                }else{
+                    $(".loadtip").html('没有更多啦！');
+                }
+
+                setTimeout(function() {
+                    for(var i = 0; i <5; i++) {
+                        oi++;
+                        $(".list-group").eq(mySwiper2.activeIndex).append('<li class="list-group-item">我是加载出来的'+oi+'...</li>');
+                    }
+                    $(".loadtip").html('上拉加载更多...');
+                    mySwiper.update(); // 重新计算高度;
+                }, 800);
+            }
+
+            // 下拉刷新
+            if(mySwiper.translate >= 50) {
+                $(".init-loading").html('正在刷新').show();
+                $(".loadtip").html('上拉加载更多');
+                loadFlag = true;
+
+                setTimeout(function() {
+                    $(".refreshtip").show(0);
+                    $(".init-loading").html('刷新成功！');
+                    setTimeout(function(){
+                        $(".init-loading").html('').hide();
+                    },800);
+                    $(".loadtip").show(0);
+
+                    //刷新操作
+                    mySwiper.update(); // 重新计算高度;
+                }, 1000);
+            }else if(mySwiper.translate >= 0 && mySwiper.translate < 50){
+                $(".init-loading").html('').hide();
+            }
+            return false;
+        }
+    });
+    $scope.mySwiper2 = new Swiper('.swiper-container2',{
+        onTransitionEnd: function(swiper){
+
+            $('.w').css('transform', 'translate3d(0px, 0px, 0px)')
+            $('.swiper-container2 .swiper-slide-active').css('height','auto').siblings('.inner-slide').css('height','0px');
+            mySwiper.update();
+
+//			$('.tab a').eq(mySwiper2.activeIndex).addClass('active').siblings('a').removeClass('active');
+        }
+
     });
 })
     // .factory('Item',function ($scope,$http) {
